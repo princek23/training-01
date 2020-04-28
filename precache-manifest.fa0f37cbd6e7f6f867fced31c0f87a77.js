a10f89c4833d50261ba49d95c7808817f846cec7
princek23 <prince@moonraft.com>
2020-04-28 12:17:10 +0530

    Updates

---
+++ precache-manifest.fa0f37cbd6e7f6f867fced31c0f87a77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbcdd0ae48217a7d680b64c7669285b",
+    "url": "/training-01/index.html"
+  },
+  {
+    "revision": "c26f17a18f3555ad3f8c",
+    "url": "/training-01/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "c35cc8ef9a199cba9e8e",
+    "url": "/training-01/static/css/main.69086536.chunk.css"
+  },
+  {
+    "revision": "c26f17a18f3555ad3f8c",
+    "url": "/training-01/static/js/2.bfc75d53.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/training-01/static/js/2.bfc75d53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c35cc8ef9a199cba9e8e",
+    "url": "/training-01/static/js/main.c4c377e2.chunk.js"
+  },
+  {
+    "revision": "863b3c4cbc16db160df8",
+    "url": "/training-01/static/js/runtime-main.dce5d6ff.js"
+  },
+  {
+    "revision": "53bc2a481f3665c5c963d62b449c4a6b",
+    "url": "/training-01/static/media/HelveticaNeue-Condensed.53bc2a48.ttf"
+  }
+]);
