758b6005ab9485a303cccb8aa5f1174e9b00ac49
princek23 <prince@moonraft.com>
2020-05-13 11:55:54 +0530

    Updates

---
+++ precache-manifest.7cbabef4a53475fead745011c87c8bc4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a37a000d0bd83892de854e2fee7fd5b",
+    "url": "/training-01/index.html"
+  },
+  {
+    "revision": "303b44db185c8eac2abb",
+    "url": "/training-01/static/css/2.d3cd085a.chunk.css"
+  },
+  {
+    "revision": "199a822160e97a17cd49",
+    "url": "/training-01/static/css/main.aa2b79ae.chunk.css"
+  },
+  {
+    "revision": "303b44db185c8eac2abb",
+    "url": "/training-01/static/js/2.6975a4a5.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/training-01/static/js/2.6975a4a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "199a822160e97a17cd49",
+    "url": "/training-01/static/js/main.4db0fc40.chunk.js"
+  },
+  {
+    "revision": "863b3c4cbc16db160df8",
+    "url": "/training-01/static/js/runtime-main.dce5d6ff.js"
+  },
+  {
+    "revision": "53bc2a481f3665c5c963d62b449c4a6b",
+    "url": "/training-01/static/media/HelveticaNeue-Condensed.53bc2a48.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/training-01/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/training-01/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/training-01/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/training-01/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/training-01/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
