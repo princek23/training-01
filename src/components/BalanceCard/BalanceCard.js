f2761c876c9ae6c08377a1dcd111dd7dbb515ee3
princek23 <prince@moonraft.com>
2020-05-26 14:17:01 +0530

    API call on Balance card

--- src/components/BalanceCard/BalanceCard.js
+++ src/components/BalanceCard/BalanceCard.js
@@ -1,27 +1,11 @@
 import React from "react";
 import classes from "./BalanceCard.module.css";
-// import style from "styled-components";
-
-// const ToolTip=style.span`
-// position: relative;
-//   display: inline-block;
-
-//     visibility: visible;
-//     position: absolute;
-//     visibility: hidden;
-
-//     visibility: visible;
-//     background-color: rgba(0, 103, 120, 0.9);
-//     color: white;
-//     width: 300.8px;
-//     height: 56px;
-//     box-shadow: 5px 0 10px 0 rgba(0, 0, 0, 0.1);
-//     padding: 20px 0 0 33px;
-//     font-size: 14px;
-// `;
 
 const balanceCard = (props) => {
-  const cardData = [...props.cardData];
+  const cardData = [
+    props.card_name,
+    props.card_balance
+  ];
 
   return (
     <div className={[classes.BalanceCard]}>
--- src/components/MainContent/AccountDetails/AccountTypes/AccountTypes.js
+++ src/components/MainContent/AccountDetails/AccountTypes/AccountTypes.js
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './AccountTypes.module.css';
 import BalaceCard from "../../../BalanceCard/BalanceCard";
 import TabView from '../../TabView/TabView';
 
-const cardData = {
-    card1: ['Current Balance', '$120,000'],
-    card2: ['Available Balance', '$ 30,000', 'VIEW TRANSACTION SUMMARY'],
-};
+// const cardData = {
+//     card1: ['Current Balance', '$120,000'],
+//     card2: ['Available Balance', '$ 30,000', 'VIEW TRANSACTION SUMMARY'],
+// };
 
-const mainTop = () => (
-    <div className={[classes.MainTop, 'MaxWidth'].join(' ')}>
-        <div>
-            <BalaceCard cardData={cardData.card1} />
-            <BalaceCard cardData={cardData.card2}><i className="fa">&#xf061;</i></BalaceCard>
-        </div>
-        <div>
-            <TabView />
-        </div>
-    </div>
-);
 
-export default mainTop;
+class AccountType extends Component{
+    state={
+        data: {
+            currentBalance: '',
+            availableBalance: ''
+        }
+    };
+
+    componentDidMount(){
+        fetch("https://demo0923316.mockable.io/balance_cards")
+        .then(resp=>resp.json())
+        .then(resp=>this.successHanlder(resp))
+    }
+
+    successHanlder=(result)=>{
+        this.setState({
+            data:result.data
+        });
+    }
+    render(){
+        return(
+            <div className={[classes.MainTop, 'MaxWidth'].join(' ')}>
+            <div>
+            <BalaceCard card_name="Current Balance" card_balance={this.state.data.currentBalance}/>
+            <BalaceCard card_name="Available Balance" card_balance={this.state.data.availableBalance}/>
+            </div>
+            <div>
+                 <TabView />
+            </div>
+            </div>
+        )
+    }
+}
+
+export default AccountType;
